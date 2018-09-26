7865703346ee1bc1a071f805ef5fa39caca3e510
DaltonHart <daltonvaren@gmail.com>
2018-09-26 15:34:44 -0700

    updated file population

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let shell = require('shelljs')
 let colors = require('colors')
 let fs = require('fs')
 let templates = require('./templates/templates.js')
+let models = require('./models/models.js')
 
 let appName = process.argv[2]
 let appDirectory = `${process.cwd()}/${appName}`
@@ -15,12 +16,29 @@ const run = async () => {
         console.log('Something went wrong while trying to create a new React app using create-react-app'.red)
         return false;
     }
-    await cdIntoNewApp()
+    await makeDirs()
     await installPackages()
     await updateTemplates()
     console.log("All Done!".green)
 }
-
+// create dirs in folder structure 
+const makeDirs = () => {
+    return new Promise(resolve => {
+        shell.cd(appDirectory)
+        shell.exec(`mkdir sass`, () => {
+            console.log(`created sass`.green)
+            shell.cd(`${appDirectory}/sass`)
+            shell.exec(`mkdir components`, () => {
+                console.log(`created sass components`.green)
+            })
+        })
+        shell.cd(`${appDirectory}/src`)
+        shell.exec(`mkdir actions config components containers models reducers store`, () => {
+            console.log(`created dirs`.green)
+        })
+        resolve()
+    })
+}
 // calls create react app with given name
 const createReactApp = () => {
     return new Promise(resolve => {
@@ -38,19 +56,11 @@ const createReactApp = () => {
     })
 }
 
-// Move into project folder
-const cdIntoNewApp = () => {
-    return new Promise(resolve => {
-        shell.exec(`cd ${appName}`, () => {
-            resolve()
-        })
-    })
-}
-
 // after move into folder install additional packages
 const installPackages = () => {
     return new Promise(resolve => {
         console.log("\nInstalling redux, react-router, react-router-dom, react-redux, axios, and redux-thunk\n".green)
+        shell.cd(appDirectory)
         shell.exec(`npm install --save redux react-router react-redux redux-thunk react-router-dom axios`, () => {
             console.log("\nFinished installing packages\n".green)
             resolve()
@@ -61,9 +71,6 @@ const installPackages = () => {
 // update the template with given files inside the templates folder
 const updateTemplates = () => {
     return new Promise(resolve => {
-        shell.exec(`mkdir actions config`, () => {
-            console.log(`created dirs`.green)
-        })
         let promises = []
         Object.keys(templates).forEach((fileName, i) => {
             promises[i] = new Promise(res => {
@@ -73,6 +80,18 @@ const updateTemplates = () => {
                     }
                     res()
                 })
+                console.log(`templates made`)
+            })
+        })
+        Object.keys(models).forEach((fileName, i) => {
+            promises[i] = new Promise(res => {
+                fs.writeFile(`${appDirectory}/src/models/${fileName}`, models[fileName], function (err) {
+                    if (err) {
+                        return console.log(err)
+                    }
+                    res()
+                })
+                console.log(`models made`)
             })
         })
         Promise.all(promises).then(() => {
--- templates/templates.js
+++ templates/templates.js
@@ -6,7 +6,7 @@ module.exports = {
     '_base.scss': require('./sass/components/_base.js'),
     '_header.scss': require('./sass/components/_header.js'),
     'index.js': require('./reducers/index.js'),
-    'Base.js': require('./models/Base.js'),
+    // 'Base.js': require('./models/Base.js'),
     'baseContainer.js': require('./containers/baseContainer.js'),
     'action-types.js': require('./constants/action-types.js'),
     'routes.js': require('./config/routes.js'),
